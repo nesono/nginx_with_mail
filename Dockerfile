bd94f8273b05956445660902a03d249a56710dbe
Jochen Issing <c.333+git@nesono.com>
2024-02-14 14:28:38 +0100

    Adding TLS configuration and setup

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,20 @@
 FROM nginx
-COPY nginx.conf /etc/nginx/nginx.conf
+LABEL maintainer="Jochen Issing <c.333+github@nesono.com> (@jochenissing)"
+
+COPY configure.sh /usr/local/bin
+RUN rm -f /etc/nginx/conf.d/default.conf
 COPY http_auth.conf /etc/nginx/conf.d/http_auth.conf
 COPY fastcgi_params /etc/nginx/fastcgi_params
 COPY nginx_auth.cgi /usr/local/bin
 RUN mkdir -p /etc/nginx/mail.d/
 COPY smtp.conf /etc/nginx/mail.d/smtp.conf
 COPY imap.conf /etc/nginx/mail.d/imap.conf
+
 COPY supervisord.conf /etc
 COPY docker_entrypoint.sh /
+
 COPY smtp_nesono_com.conf /etc/stunnel/smtp_nesono_com.conf
+
 RUN apt-get update && \
     apt-get install -y  --no-install-recommends \
     python3-pip supervisor stunnel python3-venv
@@ -16,6 +22,5 @@ RUN python3 -m venv /opt/venv && /opt/venv/bin/pip3 install flup
 RUN mkdir -p /var/run/fcgi && \
     chown nginx:nginx /var/run/fcgi
 
-RUN rm -f /etc/nginx/conf.d/default.conf
 
 ENTRYPOINT ["/docker_entrypoint.sh"]
---
+++ configure.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -o errexit -o nounset -o pipefail
+
+echo "Setting up nginx.conf" >&2
+
+cat >> /etc/nginx/nginx.conf <<EOF
+
+mail {
+    server_name mail.nesono.com;
+    auth_http localhost:8080/auth;
+
+    proxy_pass_error_message on;
+EOF
+
+if [[ -n ${TLS_CERT:-} && -n ${TLS_KEY:-} ]]; then
+  cat >> /etc/nginx/nginx.conf <<EOF
+    starttls only;
+    ssl_protocols TLSv1.2 TLSv1.3;
+    ssl_ciphers 'ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384';
+    ssl_prefer_server_ciphers off;
+    ssl_certificate      ${TLS_CERT};
+    ssl_certificate_key  ${TLS_KEY};
+    ssl_session_cache shared:SSL:10m;
+    ssl_session_timeout 10m;
+EOF
+else
+  echo "NO SSL SET UP TLS_CERT: ${TLS_CERT:-}, TLS_KEY: ${TLS_KEY:-}" >&2
+fi
+
+cat >> /etc/nginx/nginx.conf <<EOF
+
+    include /etc/nginx/mail.d/*.conf;
+}
+EOF
+
+echo "Configure DONE"
